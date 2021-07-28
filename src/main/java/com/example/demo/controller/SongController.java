57b9b41a4f2108882184c0eda52c652e3179dcbc
nguyendongminhtri <73800938+nguyendongminhtri@users.noreply.github.com>
2021-07-28 15:20:48 +0700

    update song

---
+++ src/main/java/com/example/demo/controller/SongController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.response.ResponMessage;
+import com.example.demo.model.Song;
+import com.example.demo.service.impl.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*")
+@RequestMapping("song")
+@RestController
+public class SongController {
+    @Autowired
+    SongService songService;
+    @GetMapping
+    public ResponseEntity<?> pageSong(@PageableDefault(sort = "nameSong", direction = Sort.Direction.ASC)Pageable pageable){
+        Page<Song> songPage = songService.findAll(pageable);
+        if(songPage.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(songPage, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<?> createSong(@Valid @RequestBody Song song){
+        if(song.getAvatarUrl()==null||song.getAvatarUrl().trim().isEmpty()){
+            return new ResponseEntity<>(new ResponMessage("noavatar"), HttpStatus.OK);
+        }
+        if(song.getMp3Url()==null||song.getMp3Url().trim().isEmpty()){
+            return new ResponseEntity<>(new ResponMessage("nomp3url"), HttpStatus.OK);
+        }
+        songService.save(song);
+        return new ResponseEntity<>(new ResponMessage("yes"), HttpStatus.OK);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteSong(@PathVariable Long id){
+        Optional<Song> song = songService.findById(id);
+        if(!song.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        songService.deleteById(song.get().getId());
+        return new ResponseEntity<>(new ResponMessage("yes"), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/demo/model/Song.java
@@ -0,0 +1,80 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "songs")
+public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotBlank
+    private String nameSong;
+    @NotBlank
+    @Lob
+    private String lyrics;
+    private String avatarUrl;
+    private String mp3Url;
+    @ManyToOne
+    User user;
+    public Song() {
+    }
+
+    public Song(Long id, String nameSong, String lyrics, String avatarUrl, String mp3Url, User user) {
+        this.id = id;
+        this.nameSong = nameSong;
+        this.lyrics = lyrics;
+        this.avatarUrl = avatarUrl;
+        this.mp3Url = mp3Url;
+        this.user = user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNameSong() {
+        return nameSong;
+    }
+
+    public void setNameSong(String nameSong) {
+        this.nameSong = nameSong;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
+
+    public void setLyrics(String lyrics) {
+        this.lyrics = lyrics;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getMp3Url() {
+        return mp3Url;
+    }
+
+    public void setMp3Url(String mp3Url) {
+        this.mp3Url = mp3Url;
+    }
+}
